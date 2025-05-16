e5e829fbca83ace15d4e5cef5c67ecb46c1829f4
Danil Pismenny <danil@brandymint.ru>
2025-05-16 17:56:07 +0300

    lint

--- config/initializers/posthog.rb
+++ config/initializers/posthog.rb
@@ -3,9 +3,9 @@
 # Copyright © 2023 Danil Pismenny <danil@brandymint.ru>
 
 if ENV.key? 'POSTHOG_API_KEY'
-  $posthog = PostHog::Client.new({
-                                   api_key: ENV.fetch('POSTHOG_API_KEY'),
-                                   host: 'https://eu.posthog.com',
-                                   on_error: proc { |_status, msg| Rails.logger.debug msg }
-                                 })
+  PostHogClient = PostHog::Client.new({
+                                        api_key: ENV.fetch('POSTHOG_API_KEY'),
+                                        host: 'https://eu.posthog.com',
+                                        on_error: proc { |_status, msg| Rails.logger.debug msg }
+                                      })
 end
