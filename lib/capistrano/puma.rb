5337a584fabf3428865113d87206cc8acd59f698
Danil Pismenny <danil@brandymint.ru>
2025-07-17 14:40:38 +0300

    Remove capistrano requirements

--- lib/capistrano/puma.rb
+++
@@ -1,7 +0,0 @@
-# Copyright © 2023 Danil Pismenny <danil@brandymint.ru>
-
-# frozen_string_literal: true
-
-set :puma_enable_socket_service, true
-set :puma_bind, -> { "unix://#{shared_path}/tmp/sockets/puma.sock" }
-set :puma_service_unit_env_files, -> { "#{shared_path}/.env" }
